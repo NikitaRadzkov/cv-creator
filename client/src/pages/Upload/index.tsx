b32536cea8593bee8b0ca7a3b33b7c178e633d74
Nikita Radzkou <nikita.radzkou@gmail.com>
2023-03-06 10:41:54 +0300

    Remove SSL certificate

--- client/src/pages/Upload/index.tsx
+++ client/src/pages/Upload/index.tsx
@@ -77,16 +77,16 @@ export const Upload: FC = () => {
       const formData = new FormData();
       formData.append('file', selectedFile);
       try {
-        await fetch(`https://35.228.141.206:8443/`, {
+        await fetch(`http://35.228.141.206:8443/`, {
           method: 'GET'
         });
 
-        await fetch(`https://35.228.141.206:8443/uploadPhoto`, {
+        await fetch(`http://35.228.141.206:8443/uploadPhoto`, {
           method: 'POST',
           body: formData
         });
 
-        const response = await fetch(`https://35.228.141.206:8443/save`, {
+        const response = await fetch(`http://35.228.141.206:8443/save`, {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json'
